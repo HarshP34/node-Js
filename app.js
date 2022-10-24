2dbca8d5330f4513ccac5df04d22cd3cf69a4843
HarshP34 <hupatel1005@gmail.com>
2022-10-24 11:28:14 +0530

    express task-4

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express=require('express');
 const bodyParser=require('body-parser');
 const app=express();
-app.use(bodyParser.urlencoded({extended:false}));
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><label for="title">Title:</label><input type="text"name="title"><label for="title">Size:</label><input type="text" name="size"><button type="submit">Add Product</button></form');
-})
-app.post('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/')
-})
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Hello to node js</h1>');
-})
 
+const adminRoutes=require('./routes/admin.js');
+const shopRoutes=require('./routes/shop.js');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>')
+})
 app.listen(4000);
