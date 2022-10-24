e6bed59caa869233d07a7d6249f35048ef6e79e5
HarshP34 <hupatel1005@gmail.com>
2022-10-24 10:36:02 +0530

    express task-3

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express=require('express');
+const bodyParser=require('body-parser');
 const app=express();
-app.use((req,res,next)=>{
-    console.log("in the middleware");
-    next();
+app.use(bodyParser.urlencoded({extended:false}));
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><label for="title">Title:</label><input type="text"name="title"><label for="title">Size:</label><input type="text" name="size"><button type="submit">Add Product</button></form');
 })
-app.use((req,res,next)=>{
-    console.log("in another middleware");
-    res.send({key1:33});
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/')
 })
+app.use('/',(req,res,next)=>{
+    res.send('<h1>Hello to node js</h1>');
+})
+
 app.listen(4000);
