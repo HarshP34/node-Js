46bd037899d2d10e34daf360f4397743fdc7c290
HarshP34 <hupatel1005@gmail.com>
2022-10-24 11:29:04 +0530

    routes files

---
+++ routes/admin.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const router=express.Router();
+
+router.get('/add-product',(req,res,next)=>{
+    res.send('<form action="/admin/add-product" method="POST"><label for="title">Title:</label><input type="text"name="title"><label for="title">Size:</label><input type="text" name="size"><button type="submit">Add Product</button></form');
+})
+router.post('/add-product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/shop/')
+})
+
+
+
+module.exports=router;
