88420606cd40945848fabc4cbec76e9840b74f38
HarshP34 <hupatel1005@gmail.com>
2022-10-24 09:32:42 +0530

    express task-1

---
+++ app.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const app=express();
+app.use((req,res,next)=>{
+    console.log("in the middleware");
+    next();
+})
+app.use((req,res,next)=>{
+    console.log("in another middleware");
+    res.send({key1:33});
+})
+app.listen(4000);
