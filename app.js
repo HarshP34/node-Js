f573d1e9aa40630dfdcd74b176630dd7fab916b1
HarshP34 <hupatel1005@gmail.com>
2022-10-25 12:07:38 +0530

    express task-6

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
+const path=require('path');
 const express=require('express');
 const bodyParser=require('body-parser');
 const app=express();
 
 const adminRoutes=require('./routes/admin.js');
 const shopRoutes=require('./routes/shop.js');
+const contactRoutes=require('./routes/contactus.js');
+const successRoutes=require('./routes/success.js')
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'public')))
 app.use('/admin',adminRoutes);
-app.use('/shop',shopRoutes);
+app.use(shopRoutes);
+app.use(contactRoutes);
+app.use(successRoutes);
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page not found</h1>')
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
 })
 app.listen(4000);
