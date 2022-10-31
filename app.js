71fa2dbfbedcb61978c0e46a05cf8148e2ac74eb
HarshP34 <114638409+HarshP34@users.noreply.github.com>
2022-10-31 16:25:35 +0530

    Express task-22

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const Product = require('./models/product');
 const User = require('./models/user');
-
+const Cart=require('./models/cart');
+const CartItem=require('./models/cart-item');
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -37,7 +38,10 @@ app.use(errorController.get404);
 
 Product.belongsTo(User,{constraints:true,onDelete:'CASCADE'});
 User.hasMany(Product);
-
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product,{through:CartItem});
+Product.belongsToMany(Cart,{through:CartItem});
 
 sequelize.sync().then(result=>{
   return User.findByPk(1);
@@ -50,7 +54,12 @@ sequelize.sync().then(result=>{
    return user
 }).then(user=>{
    //console.log(user);
-   app.listen(3000);
+   user.createCart()
+   .then(cart=>{
+      app.listen(3000);
+   })
+
+  
 })
 .catch(err=>console.log(err));
 
